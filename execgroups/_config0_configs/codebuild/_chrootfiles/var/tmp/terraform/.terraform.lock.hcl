3c4cb6bf22e5dabd8ec77aeea3ed579443f1c0e9
gary <gary>
2024-05-18 13:17:19 +0800

    rendered and updated 05-18-2024-13-17

---
+++ execgroups/_config0_configs/codebuild/_chrootfiles/var/tmp/terraform/.terraform.lock.hcl
@@ -0,0 +1,32 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/hashicorp/aws" {
+  version     = "5.47.0"
+  constraints = "~> 5.0"
+  hashes = [
+    "h1:oBap1Z3sKRRzRqKjkILpql/fa0gssLL/iqjDu62105I=",
+    "zh:0a22081994a733ef564fefdf9660e172af97ea2d2e34bcdaa4868300436248e8",
+    "zh:4a1be5a820a33baeaf81dc33d37e46a0f813f5395247f6449646fae7c9608a53",
+    "zh:8e156edfe38e2aed76c02e835eb6ba1fa4444feacdb824500cfdbad76abd0582",
+    "zh:9062035afc3d592dbbbd7f7cb41d4ce077e06584251aac50d116c04b4e3b4d3b",
+    "zh:a3abedd6b5640bf20d0d39ecaa938e382cbfe54dfb129c3fc291d9d695d9dffc",
+    "zh:b8ee3c37f497410a307e102638f8ce1cc1c77099498ff5438e506ce604cdf8dd",
+    "zh:c64d812bfdff6e7a6729bc035f6f8c86e69a01e9409fe1db551a233e320dc9e5",
+    "zh:c8df7f2993fdfc1bd15ab48eb280f4d11c5526c632c3beb62f014c0079715160",
+    "zh:cf413154640dcd5ee7286b43e395495f8f44d72f717cf2a20234c240cba528fa",
+    "zh:f5186377a986b96150611d891700d49125367b4ad3ab534b429305e8fdaa8dcc",
+  ]
+}
+
+provider "registry.opentofu.org/hashicorp/template" {
+  version = "2.2.0"
+  hashes = [
+    "h1:tdS0otiAtvUV8uLJWJNfcqOPo3llj7FyRzExw6X1srY=",
+    "zh:374c28bafc43cd65e578cb209efc9eee4c1cec7618f451528e928db98059e8c8",
+    "zh:6a2982e70fbc2ab2668d624c648ef2eb32243c1a1185246b03991a7a21326db9",
+    "zh:af83169c21bb13f141510a349e1f70cf7d893247a269bd71cad74dd22f1df0f5",
+    "zh:b81a5bedc91a1a81b938c393247248d6c3d1bd8ea685541f9c858908c0afb6b3",
+    "zh:de15486244af2d29d44d510d647cd6e0b1408e89952261013c572b7c9bfd744b",
+  ]
+}
---
+++ stacks/_config0_configs/ec2_ssh_upload/_main/run.py
@@ -0,0 +1,148 @@
+from config0_publisher.terraform import TFConstructor
+
+def _get_ssh_public_key(stack):
+
+    _lookup = {"must_be_one": True,
+               "resource_type": "ssh_key_pair",
+               "name": stack.key_name}
+    try:
+        results = stack.get_resource(decrypt=True,
+                                     **_lookup)[0]
+    except:
+        results = None
+
+    if results:
+        stack.logger.debug(f"found public key {_lookup}")
+
+    if not results:
+
+        _lookup = {"must_be_one": True,
+                   "resource_type": "ssh_public_key",
+                   "name": stack.key_name}
+
+        try:
+            results = stack.get_resource(decrypt=True,
+                                         **_lookup)[0]
+        except:
+            results = None
+
+        if results:
+            stack.logger.debug(f"found public key {_lookup}")
+
+    if not results:
+
+       # inputvars are more generic so
+       # we only take values key field
+
+       _lookup = {"must_be_one": True,
+                  "resource_type": "inputvars",
+                  "name": stack.key_name,
+                  "fields": ["values"],
+                  "flatten": True}
+
+       try:
+            results = stack.get_resource(decrypt=True,
+                                         **_lookup)[0]
+       except:
+           results = None
+
+       if results:
+           stack.logger.debug(f"found public key {_lookup}")
+
+    if not results:
+        raise Exception("could not retrieve public key")
+
+    public_key_hash = results.get("public_key_hash")
+    
+    if public_key_hash:
+        return public_key_hash
+
+    public_key = results.get("public_key")
+
+    if public_key:
+        return stack.b64_encode(public_key)
+
+    raise Exception("could not retrieve public key/public key hash from query")
+
+def run(stackargs):
+
+    # instantiate authoring stack
+    stack = newStack(stackargs)
+
+    # Add default variables
+    stack.parse.add_optional(key="key_name",
+                             tags="tfvar,db",
+                             types="str")
+
+    stack.parse.add_optional(key="name",
+                             tags="tfvar,db",
+                             types="str")
+
+    stack.parse.add_optional(key="public_key",
+                             tags="tfvar",
+                             types="str")
+
+    stack.parse.add_optional(key="aws_default_region",
+                             default="eu-west-1",
+                             tags="tfvar,resource,db,tf_exec_env",
+                             types="str")
+
+    # Add execgroup
+    stack.add_execgroup("config0-publish:::aws::ssh_key_upload",
+                        "tf_execgroup")
+
+    # Add substack
+    stack.add_substack('config0-publish:::tf_executor')
+
+    # Initialize Variables in stack
+    stack.init_variables()
+    stack.init_execgroups()
+    stack.init_substacks()
+
+    if not stack.get_attr("key_name") and stack.get_attr("name"):
+        stack.set_variable("key_name",
+                           stack.name,
+                           tags="tfvar,db",
+                           types="str")
+
+    if not stack.get_attr("key_name"):
+        msg = "key_name or name variable has to be set"
+        raise Exception(msg)
+
+    if not stack.get_attr("public_key") and stack.inputvars.get("public_key"):
+
+        stack.logger.debug_highlight("public key found in inputvars")
+
+        stack.set_variable("public_key",
+                           stack.inputvars["public_key"],
+                           tags="tfvar",
+                           types="str")
+
+    elif not stack.get_attr("public_key"):
+
+        stack.logger.debug_highlight("public key found in resources table")
+
+        _public_key = _get_ssh_public_key(stack)
+
+        stack.set_variable("public_key",
+                           _public_key,
+                           tags="tfvar",
+                           types="str")
+
+    if not stack.get_attr("public_key"):
+        msg = "public_key is missing"
+        raise Exception(msg)
+
+    # use the terraform constructor
+    tf = TFConstructor(stack=stack,
+                       provider="aws",
+                       execgroup_name=stack.tf_execgroup.name,
+                       resource_name=stack.key_name,
+                       resource_type="ssh_public_key",
+                       terraform_type="aws_key_pair")
+
+    # finalize the tf_executor
+    stack.tf_executor.insert(display=True,
+                             **tf.get())
+
+    return stack.get_results()
